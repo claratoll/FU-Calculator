f3b71a15ae94bb9513df1feb2d17ee41406c0643
claratoll <clara.toll@gmail.com>
2023-09-06 14:19:20 +0200

    2.0

--- calculator.js
+++ calculator.js
@@ -1,15 +1,15 @@
 const buttonElements = document.getElementsByClassName("button");
-//const signElements = document.getElementsByClassName("calc_sign");
+const calcNumberElements = document.getElementsByClassName("calc_number");
+const calcSignElements = document.getElementsByClassName("calc_sign");
 const result = document.getElementById("result");
 let firstInput = true;
-
-/*for (let i = 0; i < numberElements.length; i++) {
-  console.log(i);
-}
-
-Array.from(numberElements).forEach((element) => {
-  console.log(element);
-});*/
+let firstNumberBool = true;
+let secondNumberBool = false;
+let firstNumber = "";
+let secondNumber = "";
+let calcSign = "";
+const resultList = document.getElementById("result_list");
+const moreButton = document.getElementById("more_sign");
 
 for (const buttonElement of buttonElements) {
   buttonElement.addEventListener("click", () => {
@@ -20,26 +20,83 @@ for (const buttonElement of buttonElements) {
 const buttonListener = (button) => {
   if (button.innerText == "=") {
     calculate();
+  } else if (button.innerText == "more") {
+    moreButtonListener(moreButton);
   } else {
-    display(button.innerText);
+    let value = button.innerText;
+    result.innerText += value;
   }
+
+  if (
+    button.innerText == "+" ||
+    button.innerText == "-" ||
+    button.innerText == "*" ||
+    button.innerText == "/"
+  ) {
+    calcSign = button.innerText;
+    console.log(calcSign);
+    result.innerText = "";
+    secondNumberBool = true;
+    firstNumberBool = false;
+  }
+
+  if (firstNumberBool) {
+    firstNumber = result.innerText;
+    console.log("first number = " + firstNumber);
+  } else if (secondNumberBool) {
+    secondNumber = result.innerText;
+    console.log("secondNumber = " + secondNumber);
+  }
+};
+
+const moreButtonListener = (moreButton) => {
+  const calcSignExtras = document.getElementsByClassName("calc_sign_extra");
+  for (const element of calcSignExtras) {
+    element.classList.toggle("hide");
+  }
+  //
 };
 
 const clearResult = () => {
   result.innerText = "";
-};
-
-const display = (value) => {
-  if (firstInput) {
-    clearResult();
-    firstInput = false;
-  }
-  result.innerText += value;
+  firstNumber = "";
+  secondNumber = "";
+  calcSign = "";
 };
 
 const calculate = () => {
-  let res = eval(result.innerText);
+  let res = 0; // Initialize res as a number
 
-  display("=" + res);
+  // Convert firstNumber and secondNumber to floating-point numbers
+  const num1 = parseFloat(firstNumber);
+  const num2 = parseFloat(secondNumber);
+
+  if (calcSign == "+") {
+    res = num1 + num2;
+  } else if (calcSign == "-") {
+    res = num1 - num2;
+  } else if (calcSign == "*") {
+    res = num1 * num2;
+  } else if (calcSign == "/") {
+    if (num2 !== 0) {
+      // Check for division by zero
+      res = num1 / num2;
+    } else {
+      res = "Error: Division by zero";
+    }
+  }
+
+  firstNumberBool = true;
+  secondNumberBool = false;
+  let resVal = firstNumber + calcSign + secondNumber + "=" + res;
   firstInput = true;
+  clearResult();
+  displayResult(resVal);
+};
+
+const displayResult = (value) => {
+  const listItem = document.createElement("li");
+
+  listItem.innerText = value;
+  resultList.appendChild(listItem);
 };
